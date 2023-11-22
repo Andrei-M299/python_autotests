64c33a6f0f3847368fcc173146a2323f92d03663
Andrei-QA <143353141+Andrei-M299@users.noreply.github.com>
2023-11-22 19:21:05 +0500

    Add files via upload

--- RequestsTest/main.py
+++ RequestsTest/main.py
@@ -2,21 +2,21 @@ import requests
 
 response = requests.post(url='https://api.pokemonbattle.me:9104/pokemons',
                json={"name": "generate", "photo": "generate"},
-               headers={'trainer_token': 'bbd57079927d24e3aa4d6a48591d98a7', 'Content-Type': 'application/json'}, 
+               headers={'trainer_token': 'e5b1d17ba3647d83d7e21bdb68bc40b0', 'Content-Type': 'application/json'}, 
                timeout=5)
 print(f'Code: {response.status_code}. Message: {response.text}')
 
 response = requests.put(url='https://api.pokemonbattle.me:9104/pokemons',
-               json={"pokemon_id": "20158",
+               json={"pokemon_id": "20265",
                     "name": "Фича",
-                    "photo": "https://dolnikov.ru/pokemons/albums/016.png"},
-                    headers={'trainer_token': 'bbd57079927d24e3aa4d6a48591d98a7', 
+                    "photo": "https://dolnikov.ru/pokemons/albums/018.png"},
+                    headers={'trainer_token': 'e5b1d17ba3647d83d7e21bdb68bc40b0', 
                              'Content-Type': 'application/json'}, 
                             timeout=5)
 print(f'Code: {response.status_code}. Message: {response.text}')
 
 response = requests.post(url='https://api.pokemonbattle.me:9104/trainers/add_pokeball',
-               json={"pokemon_id": "13585"},
-               headers={'trainer_token': 'bbd57079927d24e3aa4d6a48591d98a7', 'Content-Type': 'application/json'}, 
+               json={"pokemon_id": "20265"},
+               headers={'trainer_token': 'e5b1d17ba3647d83d7e21bdb68bc40b0', 'Content-Type': 'application/json'}, 
                timeout=5)
 print(f'Code: {response.status_code}. Message: {response.text}')
